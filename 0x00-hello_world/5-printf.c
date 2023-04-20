7ef7ca518899417aa76da40ff4d5e126d19dfbdc
asmaaelmallah <elmallah.ae@gmail.com>
2023-04-20 06:19:53 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 /**
  * main intry point
  *
- * Description: A C program that prints with printf function.*
+ * Description: A C program that prints with printf function.
+ * 
  * Return: Always 0 (Success)
 */
 
