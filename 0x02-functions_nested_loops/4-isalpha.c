ab639159e6fc98d8fd3c68e44d2ebc198a202b37
asmaaelmallah <elmallah.ae@gmail.com>
2023-04-26 10:00:30 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
- * _isalpha - checks if character is a letter
- *
- * @c: takes input from other functions.
- *
- * Return: 1 is c if true else 0
+* main - check the code.
+*
+* Return: Always 0.
 */
-
-int _isalpha(int c)
-
+int main(void)
 {
-if (c >= 97 && c <= 122)
-	return (1);
-	if (c >= 65 && c <= 90)
-	return (1);
-	return (0);
+int r;
+
+r = _isalpha('H');
+_putchar(r + '0');
+r = _isalpha('o');
+_putchar(r + '0');
+r = _isalpha(108);
+_putchar(r + '0');
+r = _isalpha(';');
+_putchar(r + '0');
+_putchar('\n');
+return (0);
 }
 
