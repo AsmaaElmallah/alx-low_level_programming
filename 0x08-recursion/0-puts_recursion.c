f93355aea79338a2dec66b71d40443dc0bb981a5
asmaaelmallah <elmallah.ae@gmail.com>
2023-05-10 14:12:36 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,13 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') /* base case */
+	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
+	else
+	{
 	_putchar(*s);
 	_puts_recursion(s + 1);
+	}
 }
